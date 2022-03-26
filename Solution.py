f0d3c860b7872b5241178113416e4092210c58b5
Pilisa Gqokoma <60602000+Pilisa@users.noreply.github.com>
2022-03-26 23:52:03 +0200

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -1,8 +1,4 @@
 
-# The code is not correct because of the use of incorrect indentation,Solution is also not defined
-# In terms of effeciency the code is not too bad because there are no too much bugs in the code because the number of bugs is four
-# The coding style of this student is plain and simple because I am able to read and understand thye code as the reviewer
-# The student did not document the code 
 
 class Solution:
    def groupAnagrams(self, strs):
